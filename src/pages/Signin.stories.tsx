916be4e699209ab8bc6c72cc64c2e7238f03712a
Darlei Lippi <lippidev@gmail.com>
2022-10-13 19:56:17 -0300

    Adicionado pagena de login e criado o storybook para ela

---
+++ src/pages/Signin.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { within, userEvent, waitFor } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
+import { Signin } from './Signin'
+
+export default {
+    title: 'Pages/Sign In',
+    component: Signin,
+    args: {},
+    argTypes: {}
+} as Meta
+
+export const Default: StoryObj = {
+    play: async ({ canvasElement }) => {
+      const canvas = within(canvasElement)
+  
+      userEvent.type(canvas.getByPlaceholderText('Digite seu e-mail'), 'diego@rocketseat.com.br')
+      userEvent.type(canvas.getByPlaceholderText('******'), '12345678')
+  
+      userEvent.click(canvas.getByRole('button'))
+  
+      await waitFor(() => {
+        return expect(canvas.getByText('Login realizado!')).toBeInTheDocument()
+      })
+    }
+  }
